fd6dabd8e8e8c118e35b95035009f976f45c0877
Anurag negi <anurag.negi862@gmail.com>
2024-03-03 17:04:01 +0530

    saving data

---
+++ app/register/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hey = () => {
+  return (
+    <div className='text-white'>Hey</div>
+  )
+}
+
+export default Hey
--- components/experiment/TracingBeam.tsx
+++ components/experiment/TracingBeam.tsx
@@ -51,23 +51,16 @@ const dummyContent = [
           adipisicing laboris consectetur enim ipsum reprehenderit eu deserunt
           Lorem ut aliqua anim do. Duis cupidatat qui irure cupidatat incididunt
           incididunt enim magna id est qui sunt fugiat. Laboris do duis pariatur
-          fugiat Lorem aute sit ullamco. Qui deserunt non reprehenderit dolore
-          nisi velit exercitation Lorem qui do enim culpa. Aliqua eiusmod in
-          occaecat reprehenderit laborum nostrud fugiat voluptate do Lorem culpa
-          officia sint labore. Tempor consectetur excepteur ut fugiat veniam
-          commodo et labore dolore commodo pariatur.
+          fugiat Lorem aute sit ullamco
         </p>
         <p className="text-white">
           Dolor minim irure ut Lorem proident. Ipsum do pariatur est ad ad
-          veniam in commodo id reprehenderit adipisicing. Proident duis
-          exercitation ad quis ex cupidatat cupidatat occaecat adipisicing.
+          veniam in commodo id reprehenderit adipisicing.
         </p>
         <p className="text-white">
           Tempor quis dolor veniam quis dolor. Sit reprehenderit eiusmod
           reprehenderit deserunt amet laborum consequat adipisicing officia qui
-          irure id sint adipisicing. Adipisicing fugiat aliqua nulla nostrud.
-          Amet culpa officia aliquip deserunt veniam deserunt officia
-          adipisicing aliquip proident officia sunt.
+          irure id sint adipisicing. 
         </p>
       </>
     ),
@@ -82,16 +75,10 @@ const dummyContent = [
         <p className="text-white">
           Ex irure dolore veniam ex velit non aute nisi labore ipsum occaecat
           deserunt cupidatat aute. Enim cillum dolor et nulla sunt exercitation
-          non voluptate qui aliquip esse tempor. Ullamco ut sunt consectetur
-          sint qui qui do do qui do. Labore laborum culpa magna reprehenderit ea
-          velit id esse adipisicing deserunt amet dolore. Ipsum occaecat veniam
-          commodo proident aliqua id ad deserunt dolor aliquip duis veniam sunt.
+          non voluptate qui aliquip esse tempor.
         </p>
         <p className="text-white">
-          In dolore veniam excepteur eu est et sunt velit. Ipsum sint esse
-          veniam fugiat esse qui sint ad sunt reprehenderit do qui proident
-          reprehenderit. Laborum exercitation aliqua reprehenderit ea sint
-          cillum ut mollit.
+          In dolore veniam excepteur eu est et sunt velit. 
         </p>
       </>
     ),
@@ -106,10 +93,7 @@ const dummyContent = [
         <p className="text-white">
           Ex irure dolore veniam ex velit non aute nisi labore ipsum occaecat
           deserunt cupidatat aute. Enim cillum dolor et nulla sunt exercitation
-          non voluptate qui aliquip esse tempor. Ullamco ut sunt consectetur
-          sint qui qui do do qui do. Labore laborum culpa magna reprehenderit ea
-          velit id esse adipisicing deserunt amet dolore. Ipsum occaecat veniam
-          commodo proident aliqua id ad deserunt dolor aliquip duis veniam sunt.
+          non voluptate qui.
         </p>
       </>
     ),
